9a06afa59bdc0474873d49cada308451b4eb816c
Nonamenonsense <163110703+Nonamenonsense@users.noreply.github.com>
2024-04-11 13:32:59 +0530

    Delete Java directory

--- Java/BankingSystem/ClassesPart1/src/TeaClassStudent.java
+++
@@ -1,2 +0,0 @@
-public record TeaClassStudent(String id, String name, String dateOfBirth, String classList) {
-}
--- Java/HelloWorld/src/ThirdClass.java
+++
@@ -1,6 +0,0 @@
-public class ThirdClass {
-    public static void main (String [] args ){
-        System.out.println("Hello, AK");
-        System.out.print("Hello World");
-    }
-}
